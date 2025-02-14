42bf3df73bc1133e7a679375af500f9dc32afa13
Sanmay Paine <161416530+itzRikiz@users.noreply.github.com>
2025-02-14 23:19:53 +0530

    Update db.js
    commit password

--- URLShortener/config/db.js
+++ URLShortener/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://sanmaypaine:DNLOeHrPTPl3u4K2@cluster0.tw7q1.mongodb.net/UrlShortner"
+    "mongodb+srv://sanmaypaine:<Pass>@cluster0.tw7q1.mongodb.net/UrlShortner"
   )
   .then(() => console.log("mongo connected"))
   .catch((error) => console.error(error));
